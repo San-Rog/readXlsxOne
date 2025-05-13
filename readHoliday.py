03c229fb0354f1206a649192f674d29fe3ae2b99
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-12 23:17:22 -0300

    Update readHoliday.py

--- readHoliday.py
+++ readHoliday.py
@@ -2,8 +2,5 @@ import openpyxl
 import streamlit as st
 
 wb = openpyxl.load_workbook(‘feriadosNacionais.xlsx’)
-sheet = wb[‘Feriados’]
-
-C1 = sheet['C1'] # read direct value in cell C1
-
-st.write (C1.value)
+sheet = wb['Feriados']
+st.write (sheet)
